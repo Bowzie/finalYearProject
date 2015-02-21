e9519328a7ddb57d6f7795275ef272b88a0d225b
Bowzie <Bowzie@github.com>
2015-02-21 05:46:32 +0000

    testMusic.js performs a http get for a wav file and logs the buffer(raw wav binary data) to console.

--- Public/testMusic.js
+++ Public/testMusic.js
@@ -1,4 +1,19 @@
 window.onload = function() {
-	console.log("test");
+	var context = new AudioContext();	
+
+	var track;
+	var xmlHttpGet = new XMLHttpRequest();
+	xmlHttpGet.open("GET", '/../finalYearProject/music/track1.wav', true);
+	xmlHttpGet.responseType = "arraybuffer";
+	xmlHttpGet.send();
+
+	xmlHttpGet.onload = function() {
+		context.decodeAudioData(xmlHttpGet.response, function(buffer) {
+			track = buffer;
+			console.log(track);
+		});
+	}
+
+
 };
 	
