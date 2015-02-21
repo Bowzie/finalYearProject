825e3a93f2e5b26a82983c7840d46371e41e4574
Bowzie <Bowzie@github.com>
2015-02-21 06:17:14 +0000

    Plays file on default speakers and log the channel data.

--- Public/testMusic.js
+++ Public/testMusic.js
@@ -1,19 +1,26 @@
 window.onload = function() {
-	var context = new AudioContext();	
-
 	var track;
+	var context = new AudioContext();	
+	//Get audio track from file and get as an arraybuffer
 	var xmlHttpGet = new XMLHttpRequest();
-	xmlHttpGet.open("GET", '/../finalYearProject/music/track1.wav', true);
-	xmlHttpGet.responseType = "arraybuffer";
-	xmlHttpGet.send();
+	xmlHttpGet.open("GET", '/../finalYearProject/music/track1.wav', true); 
+	xmlHttpGet.responseType = "arraybuffer"; //arraybuffer = raw binary data
+
 
 	xmlHttpGet.onload = function() {
 		context.decodeAudioData(xmlHttpGet.response, function(buffer) {
-			track = buffer;
-			console.log(track);
+			loadData(buffer); 
 		});
 	}
 
-
+	//creates buffer from data response from server and plays file
+	function loadData(data) {
+		var track = context.createBufferSource();
+		track.buffer = data;
+		track.connect(context.destination);
+		track.start(0); //plays the contents of the wav
+		console.log(track.buffer.getChannelData(0));
+	}
+	xmlHttpGet.send();
 };
-	
+
