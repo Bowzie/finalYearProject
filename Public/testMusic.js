e36b8cc2eb72cd66563b98b472a0a468fb269c32
Bowzie <Bowzie@github.com>
2015-02-23 03:11:14 +0000

    Added try/catch when setting audio context. Implemented a low pass filter in code, not sure if it fully works yet.

--- Public/testMusic.js
+++ Public/testMusic.js
@@ -1,27 +1,63 @@
 window.onload = function() {
-	var track;
-	var context = new AudioContext();	
-	//Get audio track from file and get as an arraybuffer
-	var xmlHttpGet = new XMLHttpRequest();
-	xmlHttpGet.open("GET", '/../finalYearProject/music/track3.wav', true); 
-	xmlHttpGet.responseType = "arraybuffer"; //arraybuffer = raw binary data
+	var audioContext;
 
+	//Determine if browser used supports web audio api
+	try {
+	  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	  audioContext = new AudioContext();
+	} catch(e) {
+	  alert('Web Audio API is not supported in this browser');
+	}
+	
+	source = audioContext.createBufferSource();
+	getData();
+	//playData(source);
+	var lowPassNode = lowPassFilter(source);
+	console.log(lowPassNode);
+	playData(lowPassNode);
 
-	xmlHttpGet.onload = function() {
-		context.decodeAudioData(xmlHttpGet.response, function(buffer) {
-			loadData(buffer); 
-		});
+	function getData() {
+		//Get audio track from file and get as an arraybuffer
+		var xmlHttpGet = new XMLHttpRequest();
+		xmlHttpGet.open("GET", '/../finalYearProject/music/track1.wav', true); 
+		xmlHttpGet.responseType = "arraybuffer"; //arraybuffer = raw binary data
+
+		xmlHttpGet.onload = function() {
+			var data = xmlHttpGet.response;
+			audioContext.decodeAudioData(xmlHttpGet.response, function(buffer){
+				source.buffer = buffer;
+			},
+			function(e){"Error with decoding audio data" + e.err});
+		}
+
+		xmlHttpGet.send();
 	}
 
 	//creates buffer from data response from server and plays file
-	function loadData(data) {
-		var track = context.createBufferSource();
-		track.buffer = data;
-		track.connect(context.destination);
-		track.start(0); //plays the contents of the wav
-		console.log(track.buffer);
-		console.log(track.buffer.getChannelData(4));
+	function playData() {
+		source.connect(lowPassNode);
+		lowPassNode.connect(audioContext.destination);
+		source.start(0); //plays the contents of the wav
 	}
-	xmlHttpGet.send();
+	
+
+	function lowPassFilter(data) {
+		var track = data;
+		var lastOutput = 0.0;
+		var scriptNode;
+		scriptNode = audioContext.createScriptProcessor(4096, 1, 1);
+
+		scriptNode.onaudioprocess = function(e) {
+	        var input = e.inputBuffer.getChannelData(0);
+	        var output = e.outputBuffer.getChannelData(0);
+	        for (var i = 0; i < 4096; i++) {
+	            output[i] = (input[i] + lastOutput) / 2.0;
+	            lastOutput = output[i];
+	        }
+	        console.log(output);
+		}
+
+		return scriptNode;
+	}
 };
 
