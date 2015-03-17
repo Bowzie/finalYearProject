24eb426b52418862e2b0c116321b27d2f1aeee48
Bowzie <Bowzie@github.com>
2015-03-17 16:21:55 +0000

    recorder adds to a buffer(one channel). Stop button now creates the buffer. Play button implement for playback testing.

--- Public/testRecorder.js
+++ Public/testRecorder.js
@@ -3,6 +3,8 @@ var isRecording = false;
 var audioContext;
 var sourceNode;
 var javascriptNode;
+var recording = null;
+var audioBuffer;
 
 //getMedia function set for different browsers
 navigator.getMedia = ( navigator.mozGetUserMedia ||
@@ -10,8 +12,8 @@ navigator.getMedia = ( navigator.mozGetUserMedia ||
                    navigator.webkitGetUserMedia ||
                    navigator.msGetUserMedia);
 
-window.onload = function (){
-		
+window.onload = function ()
+{		
 	//Determine if browser used supports web audio api
 	try {
 	  window.AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -29,32 +31,43 @@ window.onload = function (){
 
 	var startRecorder = document.getElementById('recStart');
 	var stopRecorder = document.getElementById('recStop');
+	var play = document.getElementById('play');
 	startRecorder.addEventListener('click', startRecord);
 	stopRecorder.addEventListener('click', stopRecord);
+	play.addEventListener('click', playRecording);
 }
 
-function startRecord(evt) {
+function startRecord(evt) 
+{
 	evt.stopPropagation();
 	evt.preventDefault();
-	isRecording = true;
-	console.log("Starting record");
 
-    javascriptNode.onaudioprocess = function (e) {
-    	//console.log(e.inputBuffer);
-		var newSource = audioContext.createBufferSource();
-		newSource.buffer = e.inputBuffer;
-		newSource.connect(audioContext.destination);
-		newSource.start(0); //plays the contents of the wav
-    }
+	if(isRecording === true) 
+	{
+		console.log('Already recording!'); //TODO make toast?
+	}
+	else
+	{
+		isRecording = true;
+		console.log("Starting record");
+
+	    javascriptNode.onaudioprocess = function (e) {
+	    	addToRecordingBuffer(e.inputBuffer); 
+	    }		
+	}
+
 }
 
-function stopRecord(evt) {
+function stopRecord(evt) 
+{
 	evt.stopPropagation();
 	evt.preventDefault();
 	
 	if(isRecording === true) 
 	{
 		console.log("Stopping record and saving to buffer");
+        audioBuffer = audioContext.createBuffer( 1, recording.length, audioContext.sampleRate);
+        audioBuffer.getChannelData(0).set(recording, 0);
 	}
 	else {
 		console.log("Not currently recording!");
@@ -64,16 +77,50 @@ function stopRecord(evt) {
 	javascriptNode.onaudioprocess = null;
 }
 
-function setupAudio(stream) {
+function setupAudio(stream) 
+{
 	//Create nodes
 	sourceNode = audioContext.createMediaStreamSource(stream);
 	console.log(audioContext);
-	javascriptNode = audioContext.createScriptProcessor(4096, 1, 1);
+	javascriptNode = audioContext.createScriptProcessor(16384, 1, 1);
 	console.log(javascriptNode);
 
 	//Connect nodes together and to output(speakers)
 	sourceNode.connect(javascriptNode);
 	javascriptNode.connect(audioContext.destination);
+
+}
+
+function addToRecordingBuffer(inputBuffer) 
+{
+	if(recording === null)
+	{
+		recording = inputBuffer.getChannelData(0);
+		console.log(recording);
+	}
+	else
+	{
+		//Create temp buffer that adds inputbuffer to recording
+		var tempBuffer = new Float32Array(recording.length + inputBuffer.length);
+		//Set start of tempBuffer to recording
+		tempBuffer.set(recording, 0);
+		//Add inputBuffer to end 
+		tempBuffer.set(inputBuffer.getChannelData(0), recording.length);
+		//Set tempBuffer to global recording
+		recording = tempBuffer;
+		console.log(recording);
+	}
+}
+
+function playRecording() {
+	console.log(recording);
+	if(recording != null) 
+	{
+		var newSource = audioContext.createBufferSource();
+		newSource.buffer = audioBuffer;
+		newSource.connect(audioContext.destination);
+		newSource.start(0); //plays the contents of the wav
+	}
 
 }
 
