872066fb1ed7db9a716fec738dab398a15a8e248
Bowzie <Bowzie@github.com>
2015-03-24 16:46:14 +0000

    Basic line chart implemented.

--- Public/sampleCloud/libs/Charting/charting.js
+++ Public/sampleCloud/libs/Charting/charting.js
@@ -3,5 +3,27 @@ define([
 ], function(d3) {
     console.log(d3);
     var chart = d3.select('#chart');
+
+    var data = []; //dummy data to draw
+
+    for(var i = 0; i < 10; i++) {
+        data[i] = {'x':Math.floor((Math.random() * 400) + 1), 'y': Math.floor((Math.random() * 400) + 1)};
+    }
+
+    var line = d3.svg.line()
+        .x(function(d){ return d.x; })
+        .y(function(d){ return d.y; })
+        .interpolate('linear');   
+
+    var svg = chart.append('svg')
+        .attr('width', '400')
+        .attr('height', '400');
+
+    var graph = svg.append('path')
+        .attr('d', line(data))
+        .attr('stroke', 'black')
+        .attr('stroke-width', 2)
+        .attr('fill', 'none');
+
     console.log(chart);
 });
