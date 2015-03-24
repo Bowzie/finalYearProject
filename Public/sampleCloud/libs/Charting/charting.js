74401c76bf89cf7c20d32d7dc014f1e0c31353ee
Bowzie <Bowzie@github.com>
2015-03-24 15:26:22 +0000

    Added d3 to thirdpartylibs. Starting implementation in charting.

---
+++ Public/sampleCloud/libs/Charting/charting.js
@@ -0,0 +1,7 @@
+define([
+    'd3',
+], function(d3) {
+    console.log(d3);
+    var chart = d3.select('#chart');
+    console.log(chart);
+});
---
+++ Public/sampleCloud/libs/Charting/common.js
@@ -0,0 +1,6 @@
+require.config({
+    baseUrl: '/../../finalYearProject/Public/', //Base file path needs to be localhost/finalYearProject/Public
+    paths: {    //Commonly used scripts
+        d3: 'thirdPartyLibs/d3.min'
+    }
+});
